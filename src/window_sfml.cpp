a155faba6125aee7fcce9175d8a9f6e51793572f
Mayank Mukund Yadav <mmy5266@psu.edu>
2025-02-15 23:50:58 -0800

    Finally working sfml, ignore borken include error

--- src/window_sfml.cpp
+++ src/window_sfml.cpp
@@ -5,6 +5,11 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    // Create a circle
+    sf::CircleShape circle(100.f); // Radius of 100 pixels
+    circle.setFillColor(sf::Color::Blue);
+    circle.setPosition({860u, 490u}); // Center it roughly in 1920x1080
+
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -15,7 +20,8 @@ int main()
             }
         }
 
-        window.clear();
-        window.display();
+        window.clear(sf::Color::Red); // Background color
+        window.draw(circle);          // Draw the circle
+        window.display();             // Display the frame
     }
 }
