27f2baa1ec360d8b651d9fd1b2597778441650da
Mayank Mukund Yadav <mmy5266@psu.edu>
2025-02-17 01:31:30 -0800

    fully working for multiple particles

--- src/sfml_test.cpp
+++ src/sfml_test.cpp
@@ -1,24 +1,50 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <random>
+#include <cstdlib>
 #include "frame.h"
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({500u, 500u}), "CMake SFML Project");
+    int number_of_particles = 1000;
+
+    auto window = sf::RenderWindow(sf::VideoMode({1000u, 1000u}), "CMake SFML Project");
     window.setFramerateLimit(144);
     double x = window.getSize().x;
     double y = window.getSize().y;
-
-    std::cout<<x<<", "<<y<<std::endl;
-
+    
+    std::vector<sf::CircleShape> all_circles;
     
     Frame frame;
     frame.dt = 1.00/60.00;
-    frame.number_of_particles = 3;
+    frame.number_of_particles = number_of_particles;
     frame.tr = {x, 0};
     frame.bl = {0, y};
     
+    for (int i=0; i < number_of_particles; ++i) {
+        Particle p;
+        double x = rand()%800;
+        double y = rand()%800;
+        p.position = Vector{x, -y};
+        x = rand()%20;
+        y = rand()%20;
+        p.velocity = Vector{x, y};
+
+        frame.all_particles.push_back(p);
+
+        sf::CircleShape circle(p.radius);
+        circle.setFillColor(sf::Color::White);
+        circle.setPosition({p.position.x, p.position.y});
+        
+        all_circles.push_back(circle);
+
+        // display_details(p);
+
+    }
+    // for(sf::CircleShape circle: all_circles) {
+    //     std::cout<<circle.getPosition().x<<", "<<circle.getPosition().y<<std::endl;
+    // }
+    /*
     Particle p1;
     p1.position = Vector{50, -15};
     p1.velocity = Vector{20, 0};
@@ -46,9 +72,10 @@ int main()
     sf::CircleShape circle3(p3.radius); 
     circle3.setFillColor(sf::Color::White);
     circle3.setPosition({p3.position.x, p3.position.y});
-    
+    */
     // update_frame(frame);
 
+    
     while (window.isOpen()){
         while (const std::optional event = window.pollEvent()){
             if (event->is<sf::Event::Closed>())
@@ -68,13 +95,19 @@ int main()
             update_particle(frame.all_particles[i], frame.dt);
         }
 
+        window.clear(sf::Color::Black); 
+        /*
         circle1.setPosition({frame.all_particles[0].position.x, -frame.all_particles[0].position.y});
         circle2.setPosition({frame.all_particles[1].position.x, -frame.all_particles[1].position.y});
         circle3.setPosition({frame.all_particles[2].position.x, -frame.all_particles[2].position.y});
-        window.clear(sf::Color::Black); 
         window.draw(circle1);
         window.draw(circle2);
         window.draw(circle3);
+        */
+        for (int i = 0; i < number_of_particles; ++i) {
+            all_circles[i].setPosition({frame.all_particles[i].position.x, -frame.all_particles[i].position.y});
+            window.draw(all_circles[i]);
+        }
         window.display();
     }
     return 0;
